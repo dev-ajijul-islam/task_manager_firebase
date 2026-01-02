c41c0f9dfd7b84aa233972dbe7a932277c6915fe
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-02 23:46:02 +0600

    basic ColorScheme added in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -6,7 +6,18 @@ class TaskManager extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color primary = Color(0xFF3377FF);
+    Color secondary = Color(0xFF2563EB);
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: .light(
+          primary: primary,
+          secondary: secondary,
+          surface: Color(0xFFE3EDFF),
+          onSurface: Color(0xFF3A3A3C),
+          onPrimary: Color(0xFFF2F2F5),
+        ),
+      ),
       home: SplashScreen(),
     );
   }

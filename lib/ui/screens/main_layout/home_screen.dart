720733daf3212458905cfe0ef7cdd78fc522cb5b
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-06 01:17:12 +0600

    slider design created

--- lib/ui/screens/main_layout/home_screen.dart
+++ lib/ui/screens/main_layout/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:avatar_stack/animated_avatar_stack.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:linear_progress_bar/linear_progress_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -24,18 +26,104 @@ class HomeScreen extends StatelessWidget {
             CarouselSlider(
               items: [
                 Container(
+                  clipBehavior: .hardEdge,
                   margin: .all(10),
                   width: .infinity,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    color: Colors.white,
                     borderRadius: .circular(10),
                   ),
-                  child: Column(children: []),
+                  child: Column(
+                    mainAxisAlignment: .spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          width: .infinity,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                ColorScheme.of(context).primary,
+                                Colors.purpleAccent.shade100,
+                              ],
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 16),
+                            child: Column(
+                              mainAxisAlignment: .center,
+                              crossAxisAlignment: .start,
+                              children: [
+                                Text(
+                                  "Web design",
+                                  style: TextTheme.of(
+                                    context,
+                                  ).titleLarge?.copyWith(color: Colors.white),
+                                ),
+                                Text(
+                                  "5 Projects",
+                                  style: TextTheme.of(
+                                    context,
+                                  ).titleMedium?.copyWith(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: .only(left: 16,right: 5,bottom: 5),
+                          child: Column(
+                            mainAxisAlignment: .end,
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                "60%",
+                                style: TextTheme.of(
+                                  context,
+                                ).bodyLarge?.copyWith(fontSize: 17),
+                              ),
+                              Row(
+                                spacing: 5,
+                                children: [
+                                  Expanded(
+                                    flex: 2,
+                                    child: LinearProgressBar(
+                                      maxSteps: 100,
+                                      progressType: ProgressType.linear,
+                                      currentStep: 60,
+                                      progressColor: ColorScheme.of(context).primary,
+                                      backgroundColor: Colors.grey.shade100,
+                                      borderRadius: BorderRadius.circular(10),
+                                      minHeight: 5,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: AnimatedAvatarStack(
+                                      borderColor: Colors.white,
+                                      height: 30,
+                                      avatars: [
+                                        for (var n = 0; n < 5; n++)
+                                          NetworkImage(
+                                            'https://i.pravatar.cc/150?img=$n',
+                                          ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
               options: CarouselOptions(
-                aspectRatio: 2.5,
-                viewportFraction: 0.6,
+                aspectRatio: 2.4,
+                viewportFraction: 0.55,
                 disableCenter: true,
                 pageSnapping: false,
                 enableInfiniteScroll: false,

e7947b27dff9c9a659db878ce8f1474abf357325
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-06 00:16:34 +0600

    basic category slider created

--- lib/ui/screens/main_layout/home_screen.dart
+++ lib/ui/screens/main_layout/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +9,41 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: ColorScheme.of(context).onPrimary,
       body: Center(
-        child: Text("Home Screen"),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  Text("Categories", style: TextTheme.of(context).titleLarge),
+                  TextButton(onPressed: () {}, child: Text("See All")),
+                ],
+              ),
+            ),
+            CarouselSlider(
+              items: [
+                Container(
+                  margin: .all(10),
+                  width: .infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: .circular(10),
+                  ),
+                  child: Column(children: []),
+                ),
+              ],
+              options: CarouselOptions(
+                aspectRatio: 2.5,
+                viewportFraction: 0.6,
+                disableCenter: true,
+                pageSnapping: false,
+                enableInfiniteScroll: false,
+                padEnds: false,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

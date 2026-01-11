b655e3d35cac61c43fe850a5d6adddf114fb7f56
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-11 14:45:17 +0600

    create task controller created

---
+++ lib/app/modules/home/controllers/create_task_controller.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:task_manager_firebase/app/modules/home/data/models/task_model.dart';
+import 'package:task_manager_firebase/app/services/firebase_services.dart';
+
+class CreateTaskController extends GetxController {
+  var isLoading = false.obs;
+
+   Future<void> createTask({required TaskModel task}) async {
+    isLoading.value = true;
+    try {
+      await FirebaseServices.firestore.collection("tasks").doc().set(task.toJson());
+
+    } on FirebaseException catch (e) {
+      Get.snackbar("Success", "Task created successfully");
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}

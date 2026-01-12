376b7c38728f24d652ca9b289f70715ba7ec7cd2
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-12 18:27:22 +0600

    all users saved to fire store for chatting features

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:task_manager_firebase/app/modules/auth/data/models/user_model.dart';
 import 'package:task_manager_firebase/app/routes/app_routes.dart';
 import 'package:task_manager_firebase/app/services/firebase_services.dart';
 
@@ -42,8 +43,20 @@ class AuthController extends GetxController {
       await FirebaseServices.auth
           .createUserWithEmailAndPassword(email: email, password: password)
           .then((credentials) => credentials.user?.updateDisplayName(name));
+
+      final user = FirebaseServices.auth.currentUser!;
+
+      final UserModel userModel = UserModel(
+        uid: user.uid,
+        displayName: user.displayName ?? "",
+        email: user.email ?? "",
+        photoURL: user.photoURL ?? "",
+      );
+
+      await saveUserIfNotExists(userModel);
+
       Get.toNamed(AppRoutes.signInScreen);
-      Get.snackbar("Success", "sign up success");
+      Get.snackbar("Success", "Sign up success");
     } on FirebaseException catch (e) {
       Get.snackbar("Sign up failed", e.message.toString());
     } finally {
@@ -52,12 +65,10 @@ class AuthController extends GetxController {
   }
 
   ///================================= Google Sign In ===========================
-
   Future<void> signInWithGoogle() async {
     isLoading.value = true;
     try {
       final GoogleSignIn googleSignIn = GoogleSignIn.instance;
-
       await googleSignIn.initialize();
 
       final GoogleSignInAccount googleUser = await googleSignIn.authenticate();
@@ -77,6 +88,17 @@ class AuthController extends GetxController {
       );
 
       if (userCredential.user == null) return;
+
+      final user = FirebaseServices.auth.currentUser!;
+
+      final UserModel userModel = UserModel(
+        uid: user.uid,
+        displayName: user.displayName ?? "",
+        email: user.email ?? "",
+        photoURL: user.photoURL ?? "",
+      );
+
+      await saveUserIfNotExists(userModel);
 
       Get.snackbar("Success", "Sign in success");
       Get.offNamedUntil(AppRoutes.mainLayout, (route) => false);
@@ -102,5 +124,18 @@ class AuthController extends GetxController {
         idToken = await token;
       }
     });
+  }
+
+  //----------------- helper function for duplicate user check----------------
+  Future<void> saveUserIfNotExists(UserModel userModel) async {
+    final docRef = FirebaseServices.firestore
+        .collection("users")
+        .doc(userModel.uid);
+
+    final docSnap = await docRef.get();
+
+    if (!docSnap.exists) {
+      await docRef.set(userModel.toJson());
+    }
   }
 }
